dea94f8a714c4ba7f7aa075b187221f11ff18f27
FrancoSanchez2022 <fraannsanchez40@gmail.com>
2023-01-17 10:34:01 -0300

    Trabajo practico terminado

---
+++ dashboard/src/components/GenresInDb.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import Genre  from './Genre';
+
+let genres = [
+    {genre: 'Acción'},
+    {genre: 'Animación'},
+    {genre: 'Aventura'},
+    {genre: 'Ciencia Ficción'},
+    {genre: 'Comedia'},
+    {genre: 'Documental'},
+    {genre: 'Drama'},
+    {genre: 'Fantasia'},
+    {genre: 'Infantiles'},
+    {genre: 'Musical'}
+]
+
+class GenresInDb extends Component{
+
+    constructor(){
+        super()
+        this.state ={
+        genresList : []
+        }
+    }
+        componentDidMount() {
+            fetch('/api/genres')
+            .then(respuesta =>  { return respuesta.json()})
+            
+            .then(genres =>{
+            //console.log(genres)
+            this.setState({genresList: genres.data})
+            })
+            .catch(error => console.log(error))
+            }
+
+        
+
+    render () {
+
+    return (
+        <React.Fragment>
+                {/*<!-- Categories in DB -->*/}
+                <div className="col-lg-6 mb-4">						
+                    <div className="card shadow mb-4">
+                        <div className="card-header py-3">
+                            <h6 className="m-0 font-weight-bold text-gray-800">Genres in Data Base</h6>
+                        </div>
+                        <div className="card-body">
+                            <div className="row">
+                                {
+                                    this.state.genresList.map((genre,index)=>{
+                                        return  <Genre  {...genre}  key={index} />
+                                    })
+                                }
+                            </div>
+                        </div>
+                    </div>
+                </div>
+           
+        </React.Fragment>
+    )
+    }
+}
+export default GenresInDb;
